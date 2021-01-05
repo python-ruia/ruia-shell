b9422f6180c89dacaa9bb595d01d4f1312ea6e2d
howie.hu <howie6879@gmail.com>
2021-01-05 18:01:14 +0800

    Init

---
+++ examples/ipython_demo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+ Created by howie.hu at 2021/1/4.
+"""
+import IPython
+import ruia
+
+from IPython import embed
+# First import the embed function
+from IPython.terminal.embed import InteractiveShellEmbed
+from IPython.terminal.ipapp import load_default_config
+
+# Now create the IPython shell instance. Put ipshell() anywhere in your code
+# where you want it to open.
+banner = "*** Nested interpreter ***"
+exit_msg = "*** Back in main IPython ***"
+InteractiveShellEmbed.clear_instance()
+namespace = {"ruia": ruia}
+ipshell = InteractiveShellEmbed.instance(
+    banner1=banner, user_ns=namespace, exit_msg=exit_msg
+)
+
+ipshell()
