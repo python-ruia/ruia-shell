f1b342fa8eeef9b796bf65ecbdbe2bc8908f2c1d
howie6879 <howie6879@gmail.com>
2021-01-06 23:07:32 +0800

    Add config.py

--- ruia_shell/config.py
+++ ruia_shell/config.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python
 """
- Created by howie.hu at 2021/1/4.
+ Created by howie.hu at 2021/1/6.
 """
-import platform
-
-import IPython
 import ruia
 
-from IPython import embed
-# First import the embed function
-from IPython.terminal.embed import InteractiveShellEmbed
-from IPython.terminal.ipapp import load_default_config
-
-# Now create the IPython shell instance. Put ipshell() anywhere in your code
-# where you want it to open.
-banner = f"""
+BANNER = f"""
             ✨ Write less, run faster({ruia.__version__}). 
 __________      .__                .__           .__  .__   
 \______   \__ __|__|____      _____|  |__   ____ |  | |  |  
@@ -30,15 +20,5 @@ Available variables:
 
 Available functions:
     fetch(url_or_req)   :   Fetch a URL or ruia.Request
-    rhelp()             :   Tips for use
-    
+    r_help()            :   Tips for use
 """
-config = load_default_config()
-InteractiveShellEmbed.clear_instance()
-namespace = {"ruia": ruia}
-ipshell = InteractiveShellEmbed.instance(
-    banner1=banner, user_ns=namespace, config=config
-)
-
-
-ipshell()
--- ruia_shell/shell.py
+++ ruia_shell/shell.py
@@ -2,8 +2,14 @@
 """
  Created by howie.hu at 2021/1/5.
 """
+from typing import Union
 
+import ruia
+
+from IPython.terminal.embed import InteractiveShellEmbed
 from ruia import Request, Response, Spider
+
+from ruia_shell.config import BANNER
 
 
 class Shell:
@@ -11,17 +17,41 @@ class Shell:
     Shell class to debug Ruia crawler script.
     """
 
-    def __init__(self, python_shell: str = "ipython"):
+    def __init__(self, user_ns: dict = None, python_shell: str = "ipython"):
         """
         Init var
+        :param user_ns: user namespace,  set the default value to `None`
         :param python_shell: ipython only support
         """
-        self.banner = ""
+        self.banner = BANNER
+        self.user_ns = user_ns or {}
+        self.python_shell = python_shell
 
-    def fetch(self, url: str):
+    def fetch(self, url_or_request: Union[Request, str]):
         """
         Fetch target URL
-        :param url:
+        :param url_or_request:
+        :return:
+        """
+        pass
+
+    def refresh_user_ns(self):
+        """
+        Refresh the user namespace
+        :return:
+        """
+        pass
+
+    def r_help(self):
+        """
+        Help func for Ruia shell.
+        :return:
+        """
+        pass
+
+    def start(self):
+        """
+        Start a python shell for debugging
         :return:
         """
         pass
